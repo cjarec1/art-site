66f86fb1d3bb8ced1ed04495716984a7c70f9739
Caroline Jarecke <cjarec1@lsu.edu>
2019-09-11 01:39:03 -0500

    scrapped the active links for now, updating page content

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-$( '#topheader .navbar-nav a' ).on( 'click', function () {
-	$( '#topheader .navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
-	$( this ).parent( 'li' ).addClass( 'active' );
-}); 
-
 // $( '#topheader .navbar-nav a' ).on( 'click', function () {
 // 	$( '#topheader .navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
-// 	$( '#topheader .navbar-nav' ).find( 'li' ).addClass( 'active' );
+//     $( this ).parent( 'li' ).addClass( 'active' );
+//     // $( 'a:contains(Other Art)' ).parent( 'li' ).addClass( 'active' );
 // }); 
+
+//on each page, insert active in the li
+//find "other art"
+//go to parent > insert active
+
+
+//         $(document).ready(function(){
+//     $('#topheader .navbar-nav').find('li.active').removeClass('active');
+//     $( 'li:contains(Other Art)' ).css('background-color','green');
+// });
+
+
+$(function () {
+    setNavigation();
+});
+
+function setNavigation() {
+    var path = window.location.pathname;
+    
+    path = path.replace(/\/$/, "");
+    path = decodeURIComponent(path);
+
+    $(".nav a").each(function () {
+        var href = $(this).attr('href');
+        if (path.substring(0, href.length) === href) {
+            $(this).closest('li').addClass('active');
+        }
+    });
+}

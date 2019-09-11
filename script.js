396518c1ecdd8ac6ce52213d0adee4ae682419bc
Caroline Jarecke <cjarec1@lsu.edu>
2019-09-11 05:10:36 -0500

    mostly done except for sizing and hover issues

--- script.js
+++ script.js
@@ -32,3 +32,35 @@ function setNavigation() {
         }
     });
 }
+
+
+var modal = document.getElementById("myModal");
+var i;
+
+var img = document.getElementsByClassName("myImages");
+var modalImg = document.getElementById("img01");
+
+ for(i=0;i< img.length;i++)
+   {    
+    img[i].onclick = function(){
+
+    modal.style.display = "block";
+       modalImg.src = this.src;
+
+ }
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+
+span.onclick = function() { 
+   modal.style.display = "none";
+}
+
+
+
+
+//click anywhere to close
+modal.addEventListener('click', function(){
+  modal.style.display="none";
+})

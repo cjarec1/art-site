90628ff1feae822d12774626bb09a38e442f0bea
Caroline Jarecke <cjarec1@lsu.edu>
2019-09-10 22:42:17 -0500

    header works when on one page

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ $( '#topheader .navbar-nav a' ).on( 'click', function () {
 	$( '#topheader .navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
 	$( this ).parent( 'li' ).addClass( 'active' );
 }); 
+
+// $( '#topheader .navbar-nav a' ).on( 'click', function () {
+// 	$( '#topheader .navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
+// 	$( '#topheader .navbar-nav' ).find( 'li' ).addClass( 'active' );
+// }); 
